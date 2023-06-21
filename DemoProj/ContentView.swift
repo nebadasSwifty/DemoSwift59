0673ab37190c6948827b9dee8f60ddb76c615758
Kirill Sutormin <kirill.sutormin@icloud.com>
2023-06-20 20:57:23 -0700

    Initial Commit

---
+++ DemoProj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoProj
+//
+//  Created by Кирилл Сутормин on 20.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoProj/DemoProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjApp.swift
+//  DemoProj
+//
+//  Created by Кирилл Сутормин on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
